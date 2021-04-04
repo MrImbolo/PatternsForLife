dd04b6978c9e329cdfaf539a312aea9186f8276f
Daniel Zotov <myself0094@gmail.com>
2021-04-04 15:03:20 +0300

    Add project files.

---
+++ PatternsForLife/Creational/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsForLife.Creational
+{
+    public class Singleton
+    {
+        private IEnumerable<string> _items = new string[]
+        {
+            "item1", "item2", "item3"
+        };
+        private static readonly Singleton _instance;
+
+        public static Singleton Instance => _instance ?? new Singleton();
+
+        protected Singleton()
+        {
+        }
+
+        public IEnumerable<string> GetItems() => _items;
+        public IEnumerable<string> AddItems(IEnumerable<string> items) => _items = _items.Concat(items).ToArray();
+
+    }
+}
---
+++ PatternsForLife/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatternsForLife
+{
+    class Program
+    {
+        static void Main(/*string[] args*/)
+        {
+
+        }
+    }
+}
